b0304493521e9a9253804dcd6a704b2e3f64c74d
Hulgo Leonardo Jacinto Andrade <hulgoleo@gmail.com>
2022-11-21 14:27:20 +0000

    Create Comment Component

---
+++ src/components/Comment.jsx
@@ -0,0 +1,31 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment(props) {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/hulgo.png" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Hulgo Leonardo</strong>
+              <time title="21 de Novembro às 11:19h" dateTime="2022-11-21 11:19:00">Cerca de 1h atrás</time>
+            </div>
+            <button title="Deletar comentário">
+              <Trash size={20} />
+            </button>
+          </header>
+          <p>Comentario aleatório</p> 
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp size={20} />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
